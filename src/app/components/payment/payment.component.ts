c448e0655edb6540883ce662432edd79adb986e7
Kavindu Thiwanka <101232856+kavindu1thiwanka@users.noreply.github.com>
2025-02-17 21:54:27 +0530

    update

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,15 +1,22 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
   standalone: false,
 
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.scss'
+  styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent {
-
   @Input() visible: boolean = false;
-  @Input() carDetails: any = {};
+  @Input() carDetails: any = { image: '', name: '', price: 0, duration: 0 };
 
+  cardNumber: string = '';
+  expiry: string = '';
+  cvv: string = '';
+
+  processPayment() {
+    console.log("Processing payment for:", this.carDetails);
+    this.visible = false;
+  }
 }
