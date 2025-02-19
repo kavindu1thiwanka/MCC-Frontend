37698c83eb34b7cccc5b1e3bb6514d89e4230dfa
Kavindu Thiwanka <101232856+kavindu1thiwanka@users.noreply.github.com>
2025-02-19 07:19:21 +0530

    move validation

--- src/app/components/car-selection/car-selection.component.ts
+++ src/app/components/car-selection/car-selection.component.ts
@@ -1,9 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {VehicleService} from '../../shared/services/vehicle.service';
-import {AppConstant} from '../../shared/utils/app-constant';
-import {CommonService} from '../../shared/services/common.service';
-import {log} from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
 
 interface CommonFilterDto {
   sortBy: string;
@@ -34,9 +31,6 @@ export class CarSelectionComponent implements OnInit {
   selectedFilters: Record<string, string> = {};
   carList: any = [];
   selectedCar: any = {};
-  loginToProceed: boolean = false;
-  addressIsMissing: boolean = false;
-  incompleteAddress: boolean = false;
 
   filterDto: CommonFilterDto = {
     sortBy: '',
@@ -111,7 +105,7 @@ export class CarSelectionComponent implements OnInit {
     }
   ];
 
-  constructor(private route: ActivatedRoute, private router: Router, private vehicleService: VehicleService, private commonService: CommonService) {
+  constructor(private route: ActivatedRoute, private router: Router, private vehicleService: VehicleService) {
   }
 
   ngOnInit(): void {
@@ -186,29 +180,6 @@ export class CarSelectionComponent implements OnInit {
   }
 
   openCarModal(car: any) {
-
-    if (!localStorage.getItem(AppConstant.TOKEN) || localStorage.getItem(AppConstant.TOKEN) == null || localStorage.getItem(AppConstant.TOKEN) == '') {
-      this.loginToProceed = true;
-      return;
-    }
-
-    this.commonService.isAddressAvailable().then((res: any) => {
-      if (res?.status === 200) {
-        if (!res.body || res.body == '') {
-          this.addressIsMissing = true;
-          return;
-        } else {
-          if (res.body.addressLine1 || res.body.city || res.body.country) {
-            this.incompleteAddress = true;
-            return;
-          }
-        }
-      }
-    }).catch(e => {
-
-    })
-
-
     this.selectedCar = car;
   }
 }
--- src/app/components/vehicle-details/vehicle-details.component.ts
+++ src/app/components/vehicle-details/vehicle-details.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {PaymentService} from '../../shared/services/payment.service';
+import {AppConstant} from '../../shared/utils/app-constant';
+import {CommonService} from '../../shared/services/common.service';
 
 @Component({
   selector: 'app-vehicle-details',
@@ -13,12 +15,44 @@ export class VehicleDetailsComponent {
   @Input() visible: boolean = false;
   @Input() vehicle: any = { image: '', name: '', price: 0, duration: 0 };
 
-  constructor(private paymentService: PaymentService) {
+  loginToProceed: boolean = false;
+  addressIsMissing: boolean = false;
+  incompleteAddress: boolean = false;
+
+  constructor(private paymentService: PaymentService, private commonService: CommonService) {
   }
 
-  checkout() {
-    this.paymentService.createCheckoutSession(1).subscribe(({ checkoutUrl }) => {
-      window.location.href = checkoutUrl;
-    });
+  async checkout() {
+
+    await this.validateUserDetails();
+
+    if (!this.loginToProceed && !this.addressIsMissing && !this.incompleteAddress) {
+      this.paymentService.createCheckoutSession(1).subscribe(({ checkoutUrl }) => {
+        window.location.href = checkoutUrl;
+      });
+    }
+  }
+
+   async validateUserDetails() {
+    if (!localStorage.getItem(AppConstant.TOKEN) || localStorage.getItem(AppConstant.TOKEN) == null || localStorage.getItem(AppConstant.TOKEN) == '') {
+      this.loginToProceed = true;
+      return;
+    }
+
+    await this.commonService.isAddressAvailable().then((res: any) => {
+      if (res?.status === 200) {
+        if (!res.body || res.body == '') {
+          this.addressIsMissing = true;
+          return;
+        } else {
+          if (res.body.addressLine1 || res.body.city || res.body.country) {
+            this.incompleteAddress = true;
+            return;
+          }
+        }
+      }
+    }).catch(e => {
+
+    })
   }
 }
