ce0185b2a2be9fa7783dd96e6087bc9af9b546a8
Kavindu Thiwanka <101232856+kavindu1thiwanka@users.noreply.github.com>
2025-02-27 20:50:42 +0530

    update

--- src/app/components/driver-dashboard/driver-dashboard.component.ts
+++ src/app/components/driver-dashboard/driver-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DriverService } from '../../shared/services/driver.service';
 import { MatDialog } from '@angular/material/dialog';
+import { DriverService } from '../../shared/services/driver.service';
 
 @Component({
   selector: 'app-driver-dashboard',
@@ -12,11 +12,11 @@ export class DriverDashboardComponent implements OnInit {
   driver: any = {};
   upcomingRides: any[] = [];
   rideHistory: any[] = [];
-  earnings: any = { daily: 0, weekly: 0, monthly: 0 };
-  isOnline: boolean = false;
+  earnings = { daily: 0, weekly: 0, monthly: 0 };
+  isOnline = false;
 
-  displayedColumns: string[] = ['pickup', 'dropoff', 'time'];
-  displayedColumnsHistory: string[] = ['pickup', 'dropoff', 'status'];
+  displayedColumns = ['pickup', 'dropoff', 'time'];
+  displayedColumnsHistory = ['pickup', 'dropoff', 'status'];
 
   constructor(private driverService: DriverService, public dialog: MatDialog) {}
 
@@ -25,13 +25,19 @@ export class DriverDashboardComponent implements OnInit {
   }
 
   loadDriverData(): void {
-    this.driverService.getDriverDetails().then((data) => (this.driver = data));
-    this.driverService.getUpcomingRides().then((data) => (this.upcomingRides = data));
-    this.driverService.getRideHistory().then((data) => (this.rideHistory = data));
-    this.driverService.getEarnings().then((data) => (this.earnings = data));
+    Promise.all([
+      this.driverService.getDriverDetails().then((data) => (this.driver = data)),
+      this.driverService.getUpcomingRides().then((data) => (this.upcomingRides = data)),
+      this.driverService.getRideHistory().then((data) => (this.rideHistory = data)),
+      this.driverService.getEarnings().then((data) => (this.earnings = data)),
+    ]);
   }
 
   toggleStatus(): void {
     this.isOnline = !this.isOnline;
+  }
+
+  trackById(index: number, item: any): number {
+    return item.id || index;
   }
 }
